6659b125b21193d3c72717edbe13f98c5f8ba25b
Kale-8 <kaled.geme2@gmail.com>
2025-04-29 13:17:32 -0500

    feat: code finished

--- Grades system.py
+++ Grades system.py
@@ -1,40 +1,73 @@
+# Se importa "re" para usar expresiones regulares
+import re
 # Se definen las variables y se le asigna el tipo
 grade:float
 grades:list[float]
+strGrade:str
 userGrade:str
 average:float
-strGrade:str
-# Se valida que este en un rango de 0 a 100
-print('Determinar el estado de aprobacion:\n')
+value:float
+i:int
+higherGrades:list[float]
+specificGrade:float
+# Se valida que esté en un rango de 0 a 100
+print('\nDETERMINAR EL ESTADO DE APROBACIÓN\n')
 def alert():
         print('El numero ingresado es incorrecto, ingresalo nuevamente')
 while True:
     try:
-        grade = float(input('Ingresa una calificacion: '))
+        grade = float(input('Ingresa una calificación: '))
         if 0 <= grade <= 100: break
         else: alert()
     except ValueError: alert()
-# Se valida el estado de aprobacion
+# Se valida el estado de aprobación
 if grade >= 70:
-    print('Aprobado\n')
+    print('\nAprobado\n')
 elif grade >= 50 < 70:
-    print('Regular\n')
-else: print('Reprobado\n')
-# Se calcula el promedio de acuerdo a las calificaciones que se ingresen, tiene un limite de 4 calificaciones
-print('Calcular el promedio:\n')
+    print('\nRegular\n')
+else: print('\nReprobado\n')
+# Se calcula el promedio de acuerdo a las calificaciones que se ingresen
+print('CALCULAR EL PROMEDIO\n')
 grades = []
 strGrade = ''
-userGrade = input(f'Ingrese la calificacion separada por comas (Ex: "5,6,4.5,3"): ')
+userGrade = input(f'Ingrese las calificaciones separada por comas (Ej: "5,6,4.5,3"): ')
+# Se recorren las calificaciones con un "for" para agregar cada una a la lista "grades"
 for c in userGrade:
     if c == ',':
-        grades.append(float(strGrade.replace('\D','')))
+        grades.append(float(re.sub(r'\D','',strGrade)))
         strGrade = ''
     else:
         strGrade += c
-grades.append(float(strGrade))
-average = sum(grades) / 4
-print(f'El promedio de las calificaciones es: {average}\n')
-print(grades)
+grades.append(float(re.sub(r'\D','',strGrade)))
+average = sum(grades) / len(grades)
+print(f'Tus calificaciones son: {grades}')
+print(f'\nEl promedio de las calificaciones es: {average}\n')
 # Se ingresa el valor especificado
-print('Contar calificaciones mayores:\n')
-value:float = float(input('Ingresa un valor especifico para compararlo con tus calificaiones: '))
+print('CONTAR CALIFICACIONES MAYORES\n')
+while True:
+    try:
+        value = float(input('Ingresa un valor especifico para compararlo con tus calificaciones: '))
+        break
+    except ValueError: alert()
+# Se recorre la lista "grades" con un "while" para contar cuántas calificaciones son mayores que el valor especificado
+i = 0
+higherGrades = []
+while i < len(grades):
+    if grades[i] > value:
+        higherGrades.append(grades[i])
+    i += 1
+print(f'\nHay {len(higherGrades)} calificacion/es mayor/es al valor especificado: {higherGrades}\n')
+# Se ingresa la calificación específica
+print('VERIFICACIÓN Y CONTEO\n')
+while True:
+    try:
+        specificGrade = float(input('Ingresa la calificación especifica que deseas encontrar: \n'))
+        break
+    except ValueError: alert()
+# Se utiliza el "for" para recorrer la lista "grades" y poder encontrar las coincidencias de una calificación específica
+i = 0
+for c in grades:
+    if c == specificGrade:
+        i += 1
+        continue
+print(f'Se encontro/aron {i} coincidencia/s de tu calificación {specificGrade} en la lista {grades}')
